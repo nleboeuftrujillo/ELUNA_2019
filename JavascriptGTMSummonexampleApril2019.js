0147035d563bd12764bbc9ee817b5a6605e83de9
nleboeuftrujillo <44901302+nleboeuftrujillo@users.noreply.github.com>
2019-04-29 08:44:16 -0600

    js file upload

---
+++ JavascriptGTMSummonexampleApril2019.js
@@ -0,0 +1,11 @@
+/*
+Adapted from Tabatha Farney’s GTM script
+Change GTM-YourNumberHere to reflect your account
+This JavaScript file allows you to track virtual pages and click events in the Summon interface through Google Analytics deployed through Google Tag Manager.
+*/ 
+$(document).ready(function(){
+$( "body" ).prepend( "<script>var vPagePath = window.location.href;dataLayer = [{'virtualURL': vPagePath}];</script><noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-5QJLPW4' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-YourNumberHere');</script>");
+}); 
+
+  }
+});
